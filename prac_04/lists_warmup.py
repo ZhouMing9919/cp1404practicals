210dbffcd679093151ff51a27a4b168200edf658
ZhouMing <ming.zhou@my.jcu.edu.au>
2019-12-07 00:09:47 +0800

    My first commit

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,16 @@
+def main():
+    numbers = [3, 1, 4, 1, 5, 9, 2]
+
+    numbers[0] = "ten"
+
+    numbers[-1] = 1
+
+    print(numbers[2:])
+
+    if 9 in numbers:
+        print("9 is in the numbers")
+    else:
+        print("9 is not in the numbers")
+
+
+main()
