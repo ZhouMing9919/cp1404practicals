5fcb072041562a2c6ee59746a32206b1bd99f9e2
ZhouMing <ming.zhou@my.jcu.edu.au>
2019-12-13 18:58:41 +0800

    commit

--- prac_05/hex_colours.py
+++ prac_05/hex_colours.py
@@ -3,10 +3,10 @@ COLOR_CODES = {"black": "#000000", "azure1": "#f0ffff", "azure2": "#e0eeee",
                "bisque1": "#ffe4c4", "bisque2": "#eed5b7", "bisque3": "#cdb79e",
                "bisque4": "#8b7d6b"}
 
-color_name = input("Enter color name: ")
+color_name = input("Enter color name: ").lower()
 while color_name != "":
     if color_name in COLOR_CODES:
         print("The code for {} is {}".format(color_name, COLOR_CODES[color_name]))
     else:
-        print("Invalid color name")
-    color_name = input("Enter color name (blank to stop): ")
+        print("Invalid color name, please try again")
+    color_name = input("Enter color name (blank to stop): ").lower()
