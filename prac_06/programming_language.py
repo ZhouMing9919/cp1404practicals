07e3c5466c1eaaec5dc09aad721f9d2a068c3f38
ZhouMing <ming.zhou@my.jcu.edu.au>
2019-12-20 11:52:47 +0800

    my first commit

---
+++ prac_06/programming_language.py
@@ -0,0 +1,10 @@
+class ProgrammingLanguage:
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        # check if the programming language is dynamically typed or not
+        return self.typing == "Dynamic"
