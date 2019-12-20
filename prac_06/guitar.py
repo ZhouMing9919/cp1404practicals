cc689f1de07551a2560387e300ca8e8f691c7c60
ZhouMing <ming.zhou@my.jcu.edu.au>
2019-12-20 18:21:17 +0800

    final

--- prac_06/guitar.py
+++ prac_06/guitar.py
@@ -14,4 +14,14 @@ class Guitar:
         """Return a string representation of a Guitar."""
         return "{} ({}) : ${:,.2f}".format(self.name, self.year, self.cost)
 
+    def get_age(self):
+        """Get the age of a guitar based on the CURRENT_YEAR."""
+        return CURRENT_YEAR - self.year
 
+    def is_vintage(self):
+        """Determine if a Guitar is considered vintage or not based on age."""
+        return self.get_age() >= VINTAGE_AGE
+
+    def __lt__(self, other):
+        """Less than, used for sorting Guitars - by year released."""
+        return self.year < other.year
