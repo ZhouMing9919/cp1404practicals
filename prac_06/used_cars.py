6470c0f6e0333f46ad43cbbf9e4e55b38d9d667c
ZhouMing <ming.zhou@my.jcu.edu.au>
2019-12-20 11:33:09 +0800

    my second commit

--- prac_06/used_cars.py
+++ prac_06/used_cars.py
@@ -3,17 +3,17 @@ from prac_06.car import Car
 
 def main():
     """Demo test code to show how to use car class."""
-    my_car = Car(180)
+    my_car = Car("my car", 180)
     my_car.drive(30)
     print("fuel =", my_car.fuel)
     print("odo =", my_car.odometer)
     print(my_car)
 
-    print("Car {}, {}".format(my_car.fuel, my_car.odometer))
-    print("Car {self.fuel}, {self.odometer}".format(self=my_car))
+    print("{}, {}, {}".format(my_car.name, my_car.fuel, my_car.odometer))
+    print("{self.name}, {self.fuel}, {self.odometer}".format(self=my_car))
 
     # 1.Create a new Car object called "limo" that is initialised with 100 units of fuel.
-    limo = Car(100)
+    limo = Car("limo", 100)
     # 2.Add 20 more units of fuel to this new car object using the add method.
     limo.add_fuel(20)
     # 3.Print the amount of fuel in the car.
@@ -24,4 +24,5 @@ def main():
     print(limo.odometer)
 
 
-main()
+if __name__ == '__main__':
+    main()
