cf4b709586b008589c0944f4e401c05897eafcd4
ZhouMing <ming.zhou@my.jcu.edu.au>
2020-01-27 15:07:21 +0800

    my first commit

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -2,34 +2,40 @@ import os
 
 
 def main():
-    """Demo os module functions."""
+    """Change all the file names in a consistent format"""
     print("Starting directory is: {}".format(os.getcwd()))
 
     # Change to desired directory
-    os.chdir('Lyrics/Christmas')
+    os.chdir('Lyrics')
+    print("Change to desired directory: {}".format(os.getcwd()))
 
     # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
+    print(os.listdir('.'))
 
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
+    for directory_name, subdirectories, filenames in os.walk('.'):
+        # print("Directory:", directory_name)
+        # print("\tcontains subdirectories:", subdirectories)
+        # print("\tand files:", filenames)
+        # print("(Current working directory is: {})".format(os.getcwd()))
 
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
+        for filename in filenames:
+            new_name = get_fixed_filename(filename)
+            print("Renaming {} to {}".format(filename, new_name))
+
+            full_name = os.path.join(directory_name, filename)
+            new_name = os.path.join(directory_name, new_name)
+            os.rename(full_name, new_name)
 
 
 def get_fixed_filename(filename):
     """Return a 'fixed' version of filename."""
-    new_name = filename.replace(" ", "_").replace(".TXT", ".txt")
+
+    new_name = filename.replace(" ", "_").replace(".TXT", ".txt").title()
     return new_name
 
 
 def demo_walk():
     """Process all subdirectories using os.walk()."""
-    os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
         print("Directory:", directory_name)
         print("\tcontains subdirectories:", subdirectories)
@@ -38,3 +44,4 @@ def demo_walk():
 
 
 main()
+demo_walk()
